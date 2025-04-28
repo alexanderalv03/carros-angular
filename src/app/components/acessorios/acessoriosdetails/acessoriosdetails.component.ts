61f260a0eba0d4f7a581494b9fcb737005d40029
Alexander <al_emeric@hotmail.com>
2025-04-28 17:47:56 -0300

    update

--- src/app/components/acessorios/acessoriosdetails/acessoriosdetails.component.ts
+++ src/app/components/acessorios/acessoriosdetails/acessoriosdetails.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Acessorio } from '../../../models/acessorio';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { AcessorioService } from '../../../services/acessorio.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-acessoriosdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './acessoriosdetails.component.html',
   styleUrl: './acessoriosdetails.component.scss'
 })
 export class AcessoriosdetailsComponent {
 
+  @Input() acessorio: Acessorio = new Acessorio(""); // Novo acessório por padrão
+  @Output() retorno = new EventEmitter<Acessorio>(); // Emite o acessório após salvar
+
+  acessorioService = inject(AcessorioService);
+
+  save() {
+    if (this.acessorio.acessorioNome.trim().length === 0) {
+      Swal.fire({
+        title: 'Preencha o nome do acessório',
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      });
+      return;
+    }
+
+    if (this.acessorio.acessorioId > 0) {
+      // Atualizar acessório existente
+      this.acessorioService.update(this.acessorio, this.acessorio.acessorioId).subscribe({
+        next: msg => {
+          Swal.fire({ title: msg, icon: 'success' });
+          this.retorno.emit(this.acessorio);
+        },
+        error: err => {
+          Swal.fire({ title: 'Erro ao atualizar', icon: 'error' });
+        }
+      });
+    } else {
+      // Salvar novo acessório
+      this.acessorioService.save(this.acessorio).subscribe({
+        next: msg => {
+          Swal.fire({ title: msg, icon: 'success' });
+          this.retorno.emit(this.acessorio);
+        },
+        error: err => {
+          Swal.fire({ title: 'Erro ao salvar', icon: 'error' });
+        }
+      });
+    }
+  }
 }
--- src/app/components/acessorios/acessorioslist/acessorioslist.component.ts
+++ src/app/components/acessorios/acessorioslist/acessorioslist.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, TemplateRef, ViewChild } from '@angular/core';
+import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { AcessorioService } from '../../../services/acessorio.service';
+import { Acessorio } from '../../../models/acessorio';
+import Swal from 'sweetalert2';
+import { AcessoriosdetailsComponent } from '../acessoriosdetails/acessoriosdetails.component';
 
 @Component({
   selector: 'app-acessorioslist',
   standalone: true,
-  imports: [],
+  imports: [MdbModalModule, AcessoriosdetailsComponent],
   templateUrl: './acessorioslist.component.html',
   styleUrl: './acessorioslist.component.scss'
 })
 export class AcessorioslistComponent {
+  modalService = inject(MdbModalService);
+  @ViewChild("modalAcessorioDetalhe") modalAcessorioDetalhe!: TemplateRef<any>;
+  modalRef!: MdbModalRef<any>;
 
+  @Input("esconderBotoes") esconderBotoes: boolean = false;
+  @Output("retorno") retorno = new EventEmitter<any>();
+
+  acessorioService = inject(AcessorioService);
+  acessorioEdit: Acessorio = new Acessorio("");
+  listaAcessorios: Acessorio[] = [];
+
+  constructor() {
+    this.findAll();
+  }
+
+  findAll() {
+    this.acessorioService.listAll().subscribe({
+      next: lista => this.listaAcessorios = lista,
+      error: erro => {
+        Swal.fire({ title: 'Ocorreu um erro', icon: 'error', confirmButtonText: 'Ok' });
+      }
+    });
+  }
+
+  deleteAcessorioById(acessorio: Acessorio) {
+    Swal.fire({
+      title: 'Deletar registro?',
+      icon: 'warning',
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: "Sim, excluir!",
+      cancelButtonText: "Não, cancelar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.acessorioService.delete(acessorio.acessorioId).subscribe({
+          next: mensagem => {
+            Swal.fire({ title: mensagem, icon: 'success', confirmButtonText: 'Ok' });
+            this.findAll();
+          },
+          error: erro => {
+            Swal.fire({ title: 'Ocorreu um erro', icon: 'error', confirmButtonText: 'Ok' });
+          }
+        });
+      }
+    });
+  }
+
+  newAcessorio() {
+    this.acessorioEdit = new Acessorio("");
+    this.modalRef = this.modalService.open(this.modalAcessorioDetalhe);
+  }
+
+  editAcessorio(acessorio: Acessorio) {
+    this.acessorioEdit = Object.assign({}, acessorio);
+    this.modalRef = this.modalService.open(this.modalAcessorioDetalhe);
+  }
+
+  retornoDetalhe(acessorio: Acessorio) {
+    this.findAll();
+    this.modalRef.close();
+  }
+
+  selectAcessorio(acessorio: Acessorio) {
+    this.retorno.emit(acessorio);
+  }
 }
