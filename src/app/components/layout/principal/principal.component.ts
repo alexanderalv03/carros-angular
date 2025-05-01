ed39e024d816b9dd29230f57f48bccc06a7cbfc7
Alexander <al_emeric@hotmail.com>
2025-05-01 12:26:53 -0300

    update

--- src/app/components/layout/principal/principal.component.ts
+++ src/app/components/layout/principal/principal.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet],
+  imports: [MenuComponent, RouterOutlet, SidebarComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
