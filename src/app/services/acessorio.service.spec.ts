110cb09208188d321cbe19ccfdbd9f5d9bf1f4df
Alexander <al_emeric@hotmail.com>
2025-06-06 17:39:20 -0300

    update

--- src/app/services/acessorio.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AcessorioService } from './acessorio.service';
-
-describe('AcessorioService', () => {
-  it('should create an instance', () => {
-    expect(new AcessorioService()).toBeTruthy();
-  });
-});
